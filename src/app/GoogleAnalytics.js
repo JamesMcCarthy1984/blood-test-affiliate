8d8c91ac78f953282c0f9a05d3bb0f645eec92f2
James McCarthy <jamesmccarthy21@gmail.com>
2026-01-17 23:22:05 +0000

    Added Google Analytics and Google Tag Manager tracking

---
+++ src/app/GoogleAnalytics.js
@@ -0,0 +1,34 @@
+'use client'
+
+import Script from 'next/script'
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      {/* Google Tag Manager */}
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KPTX8BHG');
+        `}
+      </Script>
+
+      {/* Google Analytics 4 */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-W697B23MLV"
+      />
+      <Script id="ga-script" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-W697B23MLV');
+        `}
+      </Script>
+    </>
+  )
+}
